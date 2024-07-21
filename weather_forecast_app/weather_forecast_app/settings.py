ec4723a5d1dc319c8ba687c71c17160a70eefda6
Mikhail Sobolev <kseon@inbox.ru>
2024-07-21 06:45:57 +0400

    linters_isort_mypy_black_flake8

--- weather_forecast_app/weather_forecast_app/settings.py
+++ weather_forecast_app/weather_forecast_app/settings.py
@@ -19,7 +19,6 @@ from django.urls import reverse_lazy
 
 from env_data import db_host, db_name, db_password, db_port, db_user, log_level
 
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
--- weather_forecast_app/weather_forecast_app/urls.py
+++ weather_forecast_app/weather_forecast_app/urls.py
@@ -15,15 +15,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.conf import settings
-
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import include, path
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
 from forecast.views import CityCountList
-
 
 routers = DefaultRouter()
 routers.register("forecast", CityCountList, basename='city_count_list')
